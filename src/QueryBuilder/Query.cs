580ffc7a8290f5a144bcab0d3165feb24900439c
AliAlmasiZ <ali1385almasi@gmail.com>
2026-08-01 12:47:01 +0330

    feat: create Query Model

---
+++ src/QueryBuilder/Query.cs
@@ -0,0 +1,27 @@
+﻿namespace QueryBuilder;
+
+public class Query
+{
+    public List<string> SelectColumns = new();
+    public string FromTable = string.Empty;
+    public List<(string column, object value)> WhereEntries = new();
+
+    public Query Select(params string[] columns)
+    {
+        SelectColumns = columns.ToList();
+        return this;
+    }
+
+    public Query From(string table)
+    {
+        FromTable = table;
+        return this;
+    }
+
+    public Query Where(string column, object value)
+    {
+        WhereEntries.Add((column, value));
+        return this;
+    }
+
+}
