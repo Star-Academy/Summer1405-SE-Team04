ec276b03a05c1084d6387bc371ab2405cad2318c
AmirAli-Mahdieh <amyr27973@gmail.com>
2026-08-02 13:02:30 +0330

    feat: add unitTests for QueryModel

--- src/QueryBuilder/Query.cs
+++ src/QueryBuilder/Query.cs
@@ -7,10 +7,14 @@ public class Query
 
     public IReadOnlyList<string> SelectColumns => _selectColumns.AsReadOnly();
     public IReadOnlyList<(string column, object value)> WhereEntries => _whereEntries.AsReadOnly();
-    public string FromTable {get; private set;} = string.Empty;
+    public string FromTable { get; private set; } = string.Empty;
 
     public Query Select(params string[] columns)
     {
+        if (!Validator.ValidateParams(columns))
+            throw new ArgumentException("Columns can't be empty.");
+        if (!Validator.ValidateStringsNotEmpty(columns))
+            throw new ArgumentException("Every column must be valid name.");
         _selectColumns.Clear();
         _selectColumns.AddRange(columns);
         return this;
@@ -18,14 +22,32 @@ public class Query
 
     public Query From(string table)
     {
+        if (!Validator.ValidateStringsNotEmpty(table))
+            throw new ArgumentException("TableName must be valid name.");
         FromTable = table;
         return this;
     }
 
     public Query Where(string column, object value)
     {
+        if (!Validator.ValidateStringsNotEmpty(column))
+            throw new ArgumentException("ColumnName must be valid name.");
         _whereEntries.Add((column, value));
         return this;
+    }
+
+
+}
+public class Validator
+{
+    public static bool ValidateParams(object[] parameters)
+    {
+        return parameters.Count() != 0;
+    }
+
+    public static bool ValidateStringsNotEmpty(params string[] parameters)
+    {
+        return !parameters.Any(x => string.IsNullOrWhiteSpace(x));
     }
 
 }
