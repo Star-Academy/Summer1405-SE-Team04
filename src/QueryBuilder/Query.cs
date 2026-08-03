9d2b5ff0706e8e9d19c7c31de5df063cb51750d0
AmirAli-Mahdieh <amyr27973@gmail.com>
2026-08-03 09:50:28 +0330

    refactor: split Validator class

--- src/QueryBuilder/Query.cs
+++ src/QueryBuilder/Query.cs
@@ -38,16 +38,3 @@ public class Query
 
 
 }
-public class Validator
-{
-    public static bool ValidateParams(object[] parameters)
-    {
-        return parameters.Count() != 0;
-    }
-
-    public static bool ValidateStringsNotEmpty(params string[] parameters)
-    {
-        return !parameters.Any(x => string.IsNullOrWhiteSpace(x));
-    }
-
-}
