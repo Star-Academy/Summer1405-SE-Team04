056cae95ec8fc6f3f0089984ac5cd40225748cc1
AliAlmasiZ <ali1385almasi@gmail.com>
2026-08-11 17:52:21 +0330

    fix: lazy initialization for QueryFactories

--- src/ASP/Providers/QueryFactoryProvider.cs
+++ src/ASP/Providers/QueryFactoryProvider.cs
@@ -9,19 +9,21 @@ namespace Providers;
 public class QueryFactoryProvider : IQueryFactoryProvider
 {
     private readonly Config _config = new Config();
-    private readonly QueryFactory _postgresQueryFactory;
-    private readonly QueryFactory _sqlServerQueryFactory;
+    private readonly Lazy<QueryFactory> _postgresQueryFactory;
+    private readonly Lazy<QueryFactory> _sqlServerQueryFactory;
 
     public QueryFactoryProvider()
     {
-        _postgresQueryFactory = _createPostgresQueryFactory();
-        _sqlServerQueryFactory = _createSqlServerQueryFactory();
+        _postgresQueryFactory = new Lazy<QueryFactory>(_createPostgresQueryFactory);
+        _sqlServerQueryFactory = new Lazy<QueryFactory>(_createSqlServerQueryFactory);
     }
 
     public void Dispose()
     {
-        _postgresQueryFactory.Dispose();
-        _sqlServerQueryFactory.Dispose();
+        if (_postgresQueryFactory.IsValueCreated)
+            _postgresQueryFactory.Value.Dispose();
+        if (_sqlServerQueryFactory.IsValueCreated)
+            _sqlServerQueryFactory.Value.Dispose();
     }
 
     public QueryFactory GetQueryFactory(string dbName)
@@ -29,9 +31,11 @@ public class QueryFactoryProvider : IQueryFactoryProvider
 
         return dbName switch
         {
-            "postgres" => _postgresQueryFactory,
-            "sqlserver" => _sqlServerQueryFactory,
-            _ => throw new ArgumentException()
+            "postgres" => _postgresQueryFactory.Value,
+            "sqlserver" => _sqlServerQueryFactory.Value,
+            _ => throw new ArgumentException(
+                $"Unknown database '{dbName}'. Supported values are 'postgres' and 'sqlserver'.",
+                nameof(dbName))
         };
     }
     private QueryFactory _createPostgresQueryFactory()
