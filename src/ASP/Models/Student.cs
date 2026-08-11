3ab7ea2510de96adbff66bca2b1e8ebee7707f17
AliAlmasiZ <ali1385almasi@gmail.com>
2026-08-11 17:37:50 +0330

    fix: remove unused package and initialize Student Model

--- src/ASP/Models/Student.cs
+++ src/ASP/Models/Student.cs
@@ -2,10 +2,10 @@ namespace Models;
 
 public class Student
 {
-    public string StudentNumber {get; set;}
+    public string StudentNumber {get; set;} = string.Empty;
     public float Grade {get; set;}
-    public string FirstName {get; set;}
-    public string LastName {get; set;}
+    public string FirstName {get; set;} = string.Empty;
+    public string LastName {get; set;} = string.Empty;
     public bool IsMale {get; set;}
     public DateTime DateOfBirth {get; set;}
     public int LeftUnitsCount {get; set;}
